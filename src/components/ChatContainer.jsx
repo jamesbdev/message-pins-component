67b5b1de168a6ceab57681171b184db0cc024eb8
James Bremner <j.bremner@ymail.com>
2025-02-20 14:51:29 +0000

    add main container

---
+++ src/components/ChatContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ChatContainer = () => {
+    return (
+        <>
+           <div className="chat-container">
+            <h2>Messages</h2>
+            {/* create message icon  */}
+            {/* add search component  */}
+           </div>
+        </>
+     
+    )
+
+};
+
+export default ChatContainer;
